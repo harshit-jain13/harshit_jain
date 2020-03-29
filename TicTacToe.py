6be22530ae1dd6467be9b58f6ba113e04688315c
harshit-jain13 <61181118+harshit-jain13@users.noreply.github.com>
2020-03-29 23:20:09 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -27,7 +27,7 @@ def win_check(board, marker):
             or (board[4] == board[5] == board[6] == marker)
             or (board[7] == board[8] == board[9] == marker)
             or (board[1] == board[4] == board[7] == marker)
-            or (board[2] == board[5] == board[7] == marker)
+            or (board[2] == board[5] == board[8] == marker)
             or (board[3] == board[6] == board[9] == marker)
             or (board[1] == board[5] == board[9] == marker)
             or (board[3] == board[5] == board[7] == marker))
